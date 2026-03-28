7189dc09539a808f168c5f1272dad54360a69438
Christian Young <youngcompltd@gmail.com>
2026-03-28 09:39:02 -0400

    "remove comment"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,14 +10,6 @@ export default function RootLayout() {
         <Stack.Screen name="index" />
         <Stack.Screen name="rarity" />
         <Stack.Screen name="+not-found" />
-        {/* <Stack.Screen name="collection" /> 
-        <Stack.Screen name="piece/[id]" />
-        <Stack.Screen name="cart" />
-        <Stack.Screen name="auth" />
-        <Stack.Screen name="profile" />
-        <Stack.Screen name="rarity" />
-        <Stack.Screen name="success" />
-        <Stack.Screen name="auth/callback" />*/}
       </Stack>
     </AuthProvider>
   );
